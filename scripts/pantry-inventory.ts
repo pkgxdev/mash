7df52328f617012ef5bf810c9338d0b0ebf6c52b
Max Howell <mxcl@me.com>
2024-02-15 06:46:59 -0500

    `mash pantry inventory <pkgspec>`

---
+++ scripts/pantry-inventory.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S pkgx deno^1.40 run --ext=ts --allow-net=dist.pkgx.dev --allow-read --allow-env --allow-ffi --unstable-ffi
+
+import { hooks, semver } from "https://deno.land/x/libpkgx@v0.18/mod.ts"
+const { usePantry, useInventory } = hooks
+
+const rv: Record<string, string[]> = {}
+for (const arg of Deno.args) {
+  const found = await usePantry().find(arg)
+  const project = found[0].project
+  const pkg = { project, constraint: new semver.Range('*') }
+  const versions = await useInventory().get(pkg)
+  rv[pkg.project] = versions.map(v => v.toString())
+}
+
+if (Object.keys(rv).length > 1) {
+  console.log(JSON.stringify(rv, null, 2))
+} else for (const version of Object.entries(rv)[0][1]) {
+  console.log(version)
+}
